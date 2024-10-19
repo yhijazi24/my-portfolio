5fdf04c9ec938cb95826e36ebe465fd5eb6efbab
yhijazi24 <yahyahijazi643@gmail.com>
2024-10-19 12:50:22 +0200

    push

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // This ensures that the build output goes to the 'dist' folder
+  },
 })
