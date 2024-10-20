8e67b26311a85792a27efb19d9c069293a719a8c
yhijazi24 <yahyahijazi643@gmail.com>
2024-10-20 19:21:24 +0200

    push

--- api/models/Projects.js
+++ api/models/Projects.js
@@ -4,7 +4,7 @@ const ProjectsSchema = new mongoose.Schema(
     {
         img: { type: Array, required: true },
         title: { type: String, required: true, unique: true },
-        lang: { type: Array, required: true, unique: true },
+        lang: { type: Array, required: true},
         desc: { type: String, required: true },
         fullDesc: { type: String, required: true },
         webLink: { type: String, required: true },
