cf33e2bcb5764b54be20b920033335cafc8dff3f
yhijazi24 <yahyahijazi643@gmail.com>
2024-10-24 18:09:44 +0200

    push

--- ui/src/requestMethods.js
+++ ui/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = "https://portfolio-w14d.onrender.com";
+const BASE_URL = "my-portfolio0-drab.vercel.app";
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 const currentUser = user ? JSON.parse(user).currentUser : null;
 const TOKEN = currentUser?.accessToken || "";
